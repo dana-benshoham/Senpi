432e2bd77f85fe042ccbf45469aa33731b8300de
Adar Englert <adarenglert@gmail.com>
2024-10-30 12:58:21 +0000

    almost....

--- scripts/drop/run_app.sh
+++ scripts/drop/run_app.sh
@@ -8,10 +8,13 @@ fi
 
 # Variables
 APP_PATH="$1"
-FPGA_BIN_NAME = "b205_dummy3.bin"
+FPGA_BIN_NAME="b205_dummy3.bin"
 
 sudo uhd_find_devices
-cp "$APP_PATH/FPGA/$FPGA_BIN_NAME" "/usr/local/share/uhd/images/"
+sudo cp "$APP_PATH/FPGA/$FPGA_BIN_NAME" "/usr/local/share/uhd/images/"
 sudo sudo uhd_image_loader --args="type=b200" --fpga-path="/usr/local/share/uhd/images/$FPGA_BIN_NAME"
 
-"$APP_PATH/app_venv/bin/python" "$APP_PATH/src/main.py"
+# Activate the virtual environment
+source "$APP_PATH/app_venv/bin/activate"
+
+python "$APP_PATH/src/main.py"
